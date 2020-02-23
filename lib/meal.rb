84f4d7ebff9cded5290a888f05187b4fb1d4797e
T S <taneymail@gmail.com>
2020-02-23 01:22:30 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
 class Meal
+  
+  attr_accessor :waiter, :customer, :total, :tip 
+  
+  @@all = []
+  
+  def initialize(waiter,customer, total, tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total 
+    @tip = tip
+    @@all << self
+  end
+  
+  def self.all 
+    @@all 
+  end
 
 end
+© 2020 GitHub, I
